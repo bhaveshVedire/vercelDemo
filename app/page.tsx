b5d90a619a66c6346dd69c50443ae0948c0af30b
Bhavesh Vedire <vedire.bhavesh@cleartrip.com>
2024-04-07 15:32:32 +0530

    Updated spelling mistakes

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Hi This is Bhavesh Vedire with Yashu&nbsp;quizzes!
+            Hi This is Bhavesh Vedire with Yashashwini&nbsp;quizzes!
           </p>
         </a>
 
